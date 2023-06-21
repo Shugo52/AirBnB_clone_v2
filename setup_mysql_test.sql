bacbf7e26cad52e4df9696ba3da164a86acbd726
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-06-21 12:38:19 +0100

    test db setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,10 +2,10 @@
 -- create database for project development 'hbnb_test_db'
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create user for development 'hbnb_test'
-CREATE USER IF NOT EXISTS `hbnb_test`@`localhost` IDENTIFIED BY `hbnb_test_pwd`;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant privileges to user
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO `hbnb_test`@`localhost`;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 -- grant SELECT privileges on perfomance_schema for hbnb_test_db
-GRANT SELECT ON perfomance_schema.* TO `hbnb_test`@`localhost`;
+GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;

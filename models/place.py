d3418a9c559521ccfb2e87c98bb1bfa6aecfec0f
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-07-28 02:05:31 +0100

    Update

--- models/place.py
+++ models/place.py
@@ -10,6 +10,19 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
+if storage_type == 'db':
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True,
+                                 nullable=False),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True,
+                                 nullable=False)
+                          )
+
+
 class Place(BaseModel, Base):
     """ Defines Place class and inherits from BaseModel
 
@@ -40,6 +53,8 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=False)
         reviews = relationship('Review', backref='place',
                                cascade='all, delete, delete-orphan')
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 viewonly=False, backref='place_amenities')
     else:
         city_id = ""
         user_id = ""
--- models/state.py
+++ models/state.py
@@ -17,7 +17,8 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state')
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
     else:
         name = ''
 

ecf5796a7c6076699b942fd4113b87ac1d780767
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-07 00:22:04 +0100

    Update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,5 +7,5 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
 -- grant SELECT privileges on perfomance_schema for hbnb_dev_db
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,5 +7,5 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
 -- grant SELECT privileges on perfomance_schema for hbnb_test_db
-GRANT SELECT ON perfomance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;

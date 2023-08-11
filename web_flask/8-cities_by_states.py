0e4fb16857d0e52650bce0b8b11a28f18b29e0cb
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-11 23:32:21 +0100

    Work in progress

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,35 @@
+#!/us/bin/python3
+"""A Flask application
+Lis
+"""
+
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of cities by state"""
+    states = storage.all("State")
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)

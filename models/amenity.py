ed14e6df7a8566101071d857461bee40be5737e4
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-26 10:30:08 +0100

    Development

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Defines the Amenity class."""
+
+from place import place_amenity
 from models import storage_type
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -14,5 +17,7 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary=place_amenity,
+                                 viewonly=False, back_populates='place_amenities')
     else:
         name = ""

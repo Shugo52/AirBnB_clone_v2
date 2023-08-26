ae4ed55400fd70e7dbbee649c54de4e07a86367c
Ojoachubione  Olofu <olofuchubi@gmail.com>
2023-08-26 09:56:45 +0100

    Development

--- models/city.py
+++ models/city.py
@@ -14,12 +14,12 @@ class City(BaseModel, Base):
         state_id (str): would be State.id
         name (str): city name
     """
-    __tablename__ = 'cities'
+    __tablename__ = "cities"
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        #places = relationship('Place', backref='cities',
-                              #cascade='all, delete-orphan')
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
     else:
         name = ''
         state_id = ''
